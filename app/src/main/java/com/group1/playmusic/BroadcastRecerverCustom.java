2fa3bb0699de8a0a5be7a2d2f0538da20248b773
NguyenSonDuong <hunterdemon9x99@gmail.com>
2020-04-25 14:30:23 +0700

    -all

---
+++ app/src/main/java/com/group1/playmusic/BroadcastRecerverCustom.java
@@ -0,0 +1,13 @@
+package com.group1.playmusic;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class BroadcastRecerverCustom extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Action: "+ intent.getAction(), Toast.LENGTH_SHORT).show();
+    }
+}
